d4e20a0689b8bdc5cf4a7324bb3b06c145f2f674
olegaposh <oleg.poshiv@gmail.com>
2021-01-10 16:38:33 -0700

    added mdbreact package and a home page

---
+++ client/src/pages/home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { MDBJumbotron, MDBContainer, MDBRow, MDBCol } from "mdbreact";
+
+
+
+const Home = () => {
+
+    return (
+
+        <MDBContainer>
+
+        <MDBJumbotron className="Header bg-success text-light">
+            
+      <h1 className="text-center font-weight-bold h1 display-4">myFitBit</h1>
+      
+      
+      
+        </MDBJumbotron>
+
+        
+        </MDBContainer>
+    )
+}
+
+export default Home;
